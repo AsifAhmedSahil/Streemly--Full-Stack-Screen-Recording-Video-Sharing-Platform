95d339f48869873c00afca5c49334deaaad1f970
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-17 01:09:45 +0600

    Header section design:

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/Components/Header'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='wrapper page'>
+      <Header subHeader='Public Library' title='All Videos' />
+      <h1>welcome</h1>
+    </div>
+  )
+}
+
+export default Page
