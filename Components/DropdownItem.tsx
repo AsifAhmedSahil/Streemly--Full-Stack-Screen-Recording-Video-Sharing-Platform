56e1b76987b446fdce8d9fbf2c7fca14ea2c3f62
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-17 22:44:35 +0600

    home page ui:

--- Components/DropdownItem.tsx
+++ Components/DropdownItem.tsx
@@ -1,11 +1,47 @@
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
 
 const DropdownItem = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-        dsfddsfs
-    </div>
-  )
-}
+    <div className="relative">
+      <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+        <div className="filter-trigger">
+          <figure>
+            <Image
+              src="/assets/icons/hamburger.svg"
+              alt="menu"
+              width={14}
+              height={14}
+            />{" "}
+            Most Recent
+          </figure>
+          <Image
+            src="/assets/icons/arrow-down.svg"
+            alt="arrow-down"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
 
-export default DropdownItem
+      {
+        isOpen && (
+          <ul className="dropdown">
+            {["Most Recent","Most Liked"].map((option)=>(
+              <li key={option} className="list-item">
+                {option}
+
+              </li>
+            ))}
+
+          </ul>
+        )
+      }
+    </div>
+  );
+};
+
+export default DropdownItem;
