149fe393f1afa071802cba12d91a2e5679f337c9
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-17 23:39:32 +0600

    video page card section added:

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
 import Header from '@/Components/Header'
+import VideoCard from '@/Components/VideoCard'
+import { dummyCards } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
     <div className='wrapper page'>
       <Header subHeader='Public Library' title='All Videos' />
-      <h1>welcome</h1>
+      
+      <section className='video-grid'>
+
+        {
+          dummyCards.map((card)=>(
+            <VideoCard key={card.id} {...card}/>
+          ))
+        }
+      </section>
     </div>
   )
 }
