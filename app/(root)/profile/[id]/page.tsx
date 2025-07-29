bf06141c0f1f2e473aba7c8e25954a1263009450
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2025-07-29 22:46:50 +0600

    user profile section design and fetch all user data:

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,15 +1,51 @@
+import EmptyState from "@/Components/EmptyState";
 import Header from "@/Components/Header";
+import VideoCard from "@/Components/VideoCard";
+import { getAllVideosByUser } from "@/lib/actions/video";
+import { redirect } from "next/navigation";
+import { CiVideoOn } from "react-icons/ci";
 
+const page = async ({ params, searchParams }: ParamsWithSearch) => {
+  const { id } = await params;
+  const { query, filter } = await searchParams;
 
-const page =async ({params}:ParamsWithSearch) => {
-    const {id} = await params;
+  console.log("id",id)
+
+  const { user, videos } = await getAllVideosByUser(id,  query,filter);
+
+  console.log(user,id,"isuser")
+
+  if (!user) redirect("/404");
 
   return (
     <div className="wrapper page">
-        <Header subHeader="asifahmedsahil.007@gmail.com" title="Asif Ahmed" userImg="/assets/images/dummy.jpg"/>
-        
-    </div>
-  )
-}
+      <Header
+        subHeader={user?.email}
+        title={user?.name}
+        userImg={user?.image ?? ""}
+      />
 
-export default page
+      {videos?.length > 0 ? (
+        <section className="video-grid">
+          {videos.map(({ video, user }) => (
+            <VideoCard
+              key={video.id}
+              {...video}
+              thumbnail={video.thumbnailUrl}
+              userImg={user?.image || ""}
+              username={user?.name || ""}
+            />
+          ))}
+        </section>
+      ) : (
+        <EmptyState
+          icon={<CiVideoOn size={46} />}
+          title="No Videos avaiable yet"
+          description="Videos will shows up when you upload"
+        />
+      )}
+    </div>
+  );
+};
+
+export default page;
